453dee11a4e7a08d06aafae6c46b5ff78f080d4d
Nasreldin Aid <nasreldinaid@outlook.com>
2017-01-23 23:36:30 -0800

    Added Checkbox and Cancel Button.

--- app/src/main/java/com/nasreldinaid/caasera/MainActivity.java
+++ app/src/main/java/com/nasreldinaid/caasera/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -12,16 +13,21 @@ public class MainActivity extends AppCompatActivity {
 
 
     TextView LoginName;
+    CheckBox chBoxStayLoggedin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         LoginName = (EditText) findViewById(R.id.LoginName);
+        chBoxStayLoggedin = (CheckBox) findViewById(R.id.chBoxStayLoggedin);
     }
 
     public void btnLoginonClick(View v) {
         Log.i("Click", "Button wes clicked!");
         Log.i("Click", LoginName.getText().toString());
-        Toast.makeText(this, "Welcome to Caasera, We have succesfuly Logged in.", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "Welcome to Caasera, We have succesfuly Logged in." +
+                LoginName.getText().toString() + " " +
+                chBoxStayLoggedin.getText().toString(), Toast.LENGTH_LONG).show();
     }
 }
