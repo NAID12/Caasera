402cf7fac283f57bd20e32e4fcfc8c1f8f98f0f4
Nasreldin Aid <nasreldinaid@outlook.com>
2017-01-23 22:51:36 -0800

    Added Login Button and Password textfield.

--- app/src/main/java/com/nasreldinaid/caasera/MainActivity.java
+++ app/src/main/java/com/nasreldinaid/caasera/MainActivity.java
@@ -2,12 +2,26 @@ package com.nasreldinaid.caasera;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    TextView LoginName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LoginName = (EditText) findViewById(R.id.LoginName);
+    }
+
+    public void btnLoginonClick(View v) {
+        Log.i("Click", "Button wes clicked!");
+        Log.i("Click", LoginName.getText().toString());
+        Toast.makeText(this, "Welcome to Caasera, We have succesfuly Logged in.", Toast.LENGTH_LONG).show();
     }
 }
